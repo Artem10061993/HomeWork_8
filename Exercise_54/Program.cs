a167d78cfbf6673be753fabab364d4e046574fb5
Artem Kandakov <Temofey1993@gmail.com>
2022-09-20 11:31:58 +0500

    ДЗ к семинару №8

---
+++ Exercise_54/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 54: Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine("Введите высоту матрицы = ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длинну матрицы = ");
+int n = int.Parse(Console.ReadLine());
+int[ , ] array = new int[m,n];
+void FillArray(int [ , ] matr )
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+                Console.Write(matr[i,j] + "|");
+        }
+    Console.WriteLine();
+    }
+    
+}
+int current =0;
+int change = 0;
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(current=j+1; current < array.GetLength(1); current++)
+            {
+                
+                
+
+                    if (array[i,j] < array[i,current])
+                    {
+                        change = array[i,j];
+                        array[i,j] = array[i,current];
+                        array[i,current] = change;
+                    }
+                
+            }
+        Console.Write(array[i,j] + "|");
+        }
+    Console.WriteLine();
+    }
---
+++ Exercise_56/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("Введите высоту матрицы = ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите длинну матрицы = ");
+int n = int.Parse(Console.ReadLine());
+int[ , ] array = new int[m,n];
+void FillArray(int [ , ] matr )
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+                Console.Write(matr[i,j] + "|");
+        }
+    Console.WriteLine();
+    }
+    
+}
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int SumNumbrs=0;
+int[] arr = new int[m];
+for(int i = 0;  i < array.GetLength(0); i++)
+    {
+        SumNumbrs=0;
+        for(int j = 0; j < array.GetLength(1); j++)
+            {
+            SumNumbrs = array[i,j]+SumNumbrs;
+            arr[i]=SumNumbrs;
+            }
+
+    }
+int result = arr[0];
+int i_min =0;
+for(int i = 0;  i < arr.Length; i++)
+{
+    if(arr[i]<result)
+    {
+        result=arr[i];
+        i_min = i;
+    }
+}
+Console.WriteLine($"Наименьшая сумма ="+result+", находящейся в строке № " + (i_min+1));
